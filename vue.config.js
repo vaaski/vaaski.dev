3a1390b98c905a4f775f32bce9169c727484da16
vaaski <admin@vaa.ski>
2020-07-26 15:27:24 +0200

    merge manifest

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,86 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+  pwa: {
+    themeColor: "#19182d",
+    msTileColor: "#19182d",
+    name: "vaaski.dev",
+    manifestOptions: {
+      short_name: "vaaski",
+      icons: [
+        {
+          src: "./img/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "./img/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/apple-touch-icon-60x60.png",
+          sizes: "60x60",
+          type: "image/png",
+        },
+        {
+          src: "./img/apple-touch-icon-76x76.png",
+          sizes: "76x76",
+          type: "image/png",
+        },
+        {
+          src: "./img/apple-touch-icon-120x120.png",
+          sizes: "120x120",
+          type: "image/png",
+        },
+        {
+          src: "./img/apple-touch-icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "./img/apple-touch-icon-180x180.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          src: "./img/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "./img/msapplication-icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "./img/mstile-150x150.png",
+          sizes: "150x150",
+          type: "image/png",
+        },
+      ],
+    },
+  },
 }
