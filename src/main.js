c8ec5a88fba3b9644cdab3c4f5354434fad55188
vaaski <admin@vaa.ski>
2020-09-13 15:12:30 +0200

    add logrocket

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+import LogRocket from "logrocket"
+// cspell: disable-next-line
+LogRocket.init("4sngkz/vaaskidev")
+
+const UUIDGeneratorBrowser = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  )
+
+let uuid = localStorage.getItem("vaaski.dev-uuid")
+
+if (!uuid) {
+  uuid = UUIDGeneratorBrowser()
+  localStorage.setItem("vaaski.dev-uuid", uuid)
+}
+
+LogRocket.identify(uuid)
+
 import Vue from "vue"
 import app from "./app.vue"
 import "./registerServiceWorker"
