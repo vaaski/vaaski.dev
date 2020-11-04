e2422a0c55158dd82b86cafd202c8ac84cc62b8f
vaaski <admin@vaa.ski>
2020-11-04 20:06:25 +0100

    [minfy] add ebay shortlink

--- public/minify-toolkit/amazonShortUrl.js
+++ public/minify-toolkit/amazonShortUrl.js
@@ -1,12 +1,19 @@
-// strips unnecessary stuff from an amazon product url
-// filters out the ASIN and combines it with the location origin
+// strips unnecessary stuff from an amazon or ebay product url
+// filters out the product id and combines it with the location origin
 
-let out = location.href
-let url = location.origin
-url = url.replace("www.", "")
+let out, url
+
+url = location.origin.replace("www.", "")
 url = url.replace("https://", "http://")
-const asin = /(?:[/dp/]|$)([A-Z0-9]{10})/g
-out = asin.exec(out)[1]
-out = `${url}/dp/${out}`
+
+if (url.startsWith("http://amazon")) {
+  const asin = /(?:[/dp/]|$)([A-Z0-9]{10})/g
+  out = asin.exec(location.href)[1]
+  out = `${url}/dp/${out}`
+} else if (url.startsWith("http://ebay")) {
+  const id = /\d{12}/
+  out = id.exec(location.href)[0]
+  out = `${url}/itm/${out}`
+}
 
 prompt("short url:", out)
