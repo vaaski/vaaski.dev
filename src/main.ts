fb5fe6c53e08fe3c093ed9a4df64f5964a709338
vaaski <admin@vaa.ski>
2020-12-13 18:06:27 +0100

    remove logrocket

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,3 @@
-import LogRocket from "logrocket"
-// cspell: disable-next-line
-LogRocket.init("4sngkz/vaaskidev")
-
-const UUIDGeneratorBrowser = () =>
-  "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: any) =>
-    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-  )
-
-let uuid = localStorage.getItem("vaaski.dev-uuid")
-
-if (!uuid) {
-  uuid = UUIDGeneratorBrowser()
-  localStorage.setItem("vaaski.dev-uuid", uuid)
-}
-
-LogRocket.identify(uuid)
-
 import Vue from "vue"
 import app from "./app.vue"
 import "./registerServiceWorker"
