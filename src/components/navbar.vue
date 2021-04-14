be2bb2a539c576e0b158160f363215657c918a17
vaaski <admin@vaa.ski>
2021-04-14 10:30:23 +0200

    feat: logo in navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="flex h-full mx-auto w-full max-w-5xl items-center">
-    <div id="left">vaaski</div>
+    <div id="left" class="flex h-full justify-center">
+      <logo class="m-auto h-1/2" />
+    </div>
     <div id="spacer" class="flex-grow"></div>
     <div id="right">github</div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import logo from "@/components/logo.vue"
+
+export default defineComponent({
+  components: { logo },
+  setup() {},
+})
+</script>
