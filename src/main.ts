e1750b6d638d632520ba1c04cdf704e25e50f1b1
vaaski <admin@vaa.ski>
2021-04-25 18:51:14 +0200

    feat: pwa

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,19 @@ import "./styles"
 import "virtual:windi.css"
 import "vite-plugin-svg-icons/register"
 
+import { registerSW } from "virtual:pwa-register"
+
+// TODO
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log("onNeedRefresh")
+    // show a prompt to user
+  },
+  onOfflineReady() {
+    console.log("onOfflineReady")
+    // show a ready to work offline to user
+  },
+})
+
 createApp(app).use(router).mount("#app")
--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,45 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import WindiCSS from "vite-plugin-windicss"
 import viteSvgIcons from "vite-plugin-svg-icons"
+import { VitePWA } from "vite-plugin-pwa"
+
+const color = "#162837"
 
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: color,
+        background_color: color,
+        name: "vaaski.dev",
+        short_name: "vaaski.dev",
+        start_url: "./index.html",
+        display: "standalone",
+        icons: [
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+          },
+          {
+            src: "icons/pwa-256x256.png",
+            sizes: "256x256",
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
     WindiCSS({
       scan: {
         fileExtensions: ["vue", "html", "ts"],
       },
     }),
     viteSvgIcons({
-      // Specify the icon folder to be cached
       iconDirs: [resolve(process.cwd(), "src/icons")],
-      // Specify symbolId format
       symbolId: "icon-[dir]-[name]",
     }),
   ],
