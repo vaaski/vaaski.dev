53c1cec1572ed90f34f679a7e7d8ac9258cb8f8c
Oskar Sommer <oskar.sommer@t-systems.com>
2024-08-08 17:00:56 +0200

    add shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
   router: {
     options: { scrollBehaviorType: "smooth" },
   },
+
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
 })
