b26234c7af6e74d21803756cd148dd196ae229de
Oskar Sommer <oskar.sommer@t-systems.com>
2024-08-08 15:10:15 +0200

    nav link hover

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav-bar">
     <div class="left">
-      <AutoLink to="/">vaaski.dev</AutoLink>
+      <AutoLink class="wordmark" to="/">vaaski.dev</AutoLink>
     </div>
     <div class="spacer"></div>
     <div class="right">
@@ -22,11 +22,6 @@
   width: 100%;
   max-width: min(750px, 90vw);
 
-  font-style: normal;
-  font-weight: 400;
-  font-size: 1.1rem;
-  color: hsla(0, 0%, 100%, 0.5);
-
   background: linear-gradient(
     216.04deg,
     rgba(255, 255, 255, 0.06) 18.89%,
@@ -45,5 +40,28 @@
   display: flex;
   gap: 1rem;
   margin: 0 2rem;
+}
+
+a {
+  --background-x-pad: 0.75em;
+  --background-y-pad: 0.2em;
+
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.1rem;
+  color: hsla(0, 0%, 100%, 0.5);
+  padding: var(--background-y-pad) var(--background-x-pad);
+  margin: calc(var(--background-y-pad) * -1) calc(var(--background-x-pad) * -1);
+  border-radius: 1em;
+  transition: 75ms linear;
+  transition-property: color, background, text-shadow, box-shadow;
+
+  &:not(.wordmark):where(:hover, :focus, :active),
+  &.wordmark:where(:focus, :active) {
+    color: hsla(0, 0%, 100%, 1);
+    background: hsla(0, 0%, 100%, 0.1);
+    text-shadow: 0 0 15px hsla(0, 0%, 100%, 0.75);
+    box-shadow: 0 0 10px hsla(0, 0%, 100%, 0.125);
+  }
 }
 </style>
