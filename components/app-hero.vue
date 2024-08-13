7bbca0a5dbd9a1bc38e51a54ecc625cc9d7e025b
Oskar Sommer <oskar.sommer@t-systems.com>
2024-08-13 17:05:10 +0200

    massive pile of css garbage

--- components/app-hero.vue
+++ components/app-hero.vue
@@ -8,10 +8,12 @@ const { width, height } = useElementSize(hero)
   <div id="app-hero" ref="hero">
     <div class="content">
       <VaaskiLogo class="logo" />
-      <div class="tagline">
-        i build stuff for the web
-        <br />
-        using typescript, vue and go
+      <div class="tagline-shadow">
+        <div class="tagline">
+          i build stuff for the web
+          <br />
+          using typescript, vue and go
+        </div>
       </div>
     </div>
     <div class="backdrop"></div>
@@ -22,6 +24,10 @@ const { width, height } = useElementSize(hero)
 </template>
 
 <style scoped lang="scss">
+:global(:root) {
+  --hero-animation-delay: 250ms;
+}
+
 #app-hero {
   height: 100svh;
   display: flex;
@@ -29,66 +35,141 @@ const { width, height } = useElementSize(hero)
   align-items: center;
   position: relative;
 
-  .starry-backdrop {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    pointer-events: none;
-
-    z-index: 1;
-  }
-  .backdrop {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    pointer-events: none;
-
-    z-index: 1;
-    opacity: 0.5;
-    background: radial-gradient(
-        91.96% 141.6% at -22.42% 69.4%,
-        rgba(127, 0, 255, 0.46) 0%,
-        rgba(0, 0, 0, 0) 100%
-      ),
-      radial-gradient(
-        84.26% 129.74% at 50% 125.36%,
-        rgba(255, 0, 230, 0.46) 0%,
-        rgba(0, 0, 0, 0) 100%
-      ),
-      radial-gradient(
-        58.7% 90.38% at 113.99% 96.18%,
-        rgba(255, 0, 107, 0.43) 0%,
-        rgba(0, 0, 0, 0) 100%
-      ),
-      radial-gradient(
-        56.68% 87.27% at 27.94% 112.07%,
-        rgba(0, 255, 224, 0.43) 0%,
-        rgba(0, 0, 0, 0) 100%
-      );
-  }
+  --tagline-animation-delay: calc(750ms + var(--hero-animation-delay));
 }
 
+.starry-backdrop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+
+  z-index: 1;
+}
+
+@keyframes backdrop-fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.5;
+  }
+}
+.backdrop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  animation: backdrop-fade-in var(--hero-animation-delay) cubic-bezier(0.76, 0, 0.24, 1)
+    forwards;
+
+  z-index: 1;
+  opacity: 0.5;
+  background: radial-gradient(
+      91.96% 141.6% at -22.42% 69.4%,
+      rgba(127, 0, 255, 0.46) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    radial-gradient(
+      84.26% 129.74% at 50% 125.36%,
+      rgba(255, 0, 230, 0.46) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    radial-gradient(
+      58.7% 90.38% at 113.99% 96.18%,
+      rgba(255, 0, 107, 0.43) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    radial-gradient(
+      56.68% 87.27% at 27.94% 112.07%,
+      rgba(0, 255, 224, 0.43) 0%,
+      rgba(0, 0, 0, 0) 100%
+    );
+}
+
+@keyframes content-animation {
+  0% {
+    gap: 0;
+  }
+  100% {
+    gap: 4rem;
+  }
+}
 .content {
   z-index: 4;
   display: flex;
   align-items: center;
-  gap: 4rem;
+  gap: 0;
+
+  animation: content-animation 1000ms cubic-bezier(0.76, 0, 0.24, 1) forwards;
+  animation-delay: var(--tagline-animation-delay);
 }
 
+@keyframes logo-animation {
+  0% {
+    transform: scale(1.333);
+  }
+  50% {
+    transform: scale(1.333);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 .logo {
   max-width: min(320px, 75vw);
+  transform: scale(1.2);
+  animation: logo-animation 1500ms cubic-bezier(0.76, 0, 0.24, 1) forwards;
+  animation-delay: var(--hero-animation-delay);
 }
 
+.tagline-shadow {
+  filter: drop-shadow(0 0 32px hsla(274, 100%, 90%, 0.5));
+}
+
+@keyframes tagline-animation {
+  from {
+    width: 0ch;
+    height: 3em;
+    opacity: 0;
+  }
+  to {
+    width: 18.6ch;
+    height: 3em;
+    opacity: 1;
+  }
+}
+@media (max-width: 900px) {
+  @keyframes tagline-animation {
+    from {
+      height: 0;
+      width: 18.6ch;
+      opacity: 0;
+    }
+    to {
+      height: 3em;
+      width: 18.6ch;
+      opacity: 1;
+    }
+  }
+}
 .tagline {
-  max-width: 80vw;
   font-size: 2.5rem;
   font-weight: 250;
   color: hsla(0, 0%, 100%, 0.75);
-  text-shadow: 0 0 36px hsla(0, 0%, 100%, 0.25);
+
+  width: 0;
+  height: 0;
+  opacity: 0;
+  overflow: hidden;
+  text-wrap: nowrap;
+
+  animation: tagline-animation 1000ms cubic-bezier(0.76, 0, 0.24, 1) forwards;
+  animation-delay: var(--tagline-animation-delay);
 }
 
 .bottom-rounder {
