e775b086a1adeec516dad06c54c3613e81dbec23
Oskar Sommer <oskar.sommer@t-systems.com>
2024-08-13 15:50:24 +0200

    rip all the animation jazz

--- components/vaaski-logo.vue
+++ components/vaaski-logo.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import anime from "animejs"
-
-const delay = ref(true)
-
 const logoStages = [
   "M.5 0 .5 0 .5 0 .5 0 .5 0 .5 0 .5 0 .5 0 .5 0 43.9409 0 43.9409 0 43.9409 0 .5 0 .5 0 .5 0 .5 0 .5 0 .5 0Z",
   "M96.0308 186 96.0308 186 96.0308 186 96.0308 186 96.0308 186 96.0308 186 96.0308 186 96.0308 186 .5 0 43.9409 0 96.0379 101.889 96.0379 101.889 96.0308 186 96.0308 186 96.0308 186 96.0308 186 96.0308 186 96.0308 186Z",
@@ -11,56 +7,10 @@ const logoStages = [
   "M239.394 45.6091 218.422 86.2804 218.422 86.2804 205.508 111.348 243.678 186 200.237 186 148.365 84.5494 96.0308 186 .5 0 43.9409 0 96.0379 101.889 148.135 0 191.576 0 170.085 42.0683 182.144 65.654 215.969 0 239.394 45.6091 239.394 45.6091Z",
   "M311.5 186 268.059 186 218.422 86.2804 205.508 111.348 243.678 186 200.237 186 148.365 84.5494 96.0308 186 .5 0 43.9409 0 96.0379 101.889 148.135 0 191.576 0 170.085 42.0683 182.144 65.654 215.969 0 239.394 45.6091 311.5 186Z",
 ]
-
-const duration = 2000
-const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a
-const clamp = (a: number, min = 0, max = 1) => Math.min(max, Math.max(min, a))
-const invlerp = (x: number, y: number, a: number) => clamp((a - x) / (y - x))
-const range = (x1: number, y1: number, x2: number, y2: number, a: number) =>
-  lerp(x2, y2, invlerp(x1, y1, a))
-
-const easing = (x: number) => {
-  return 1 - (1 - x) ** 4
-}
-
-const path = ref<SVGPathElement>()
-onMounted(() => {
-  if (!path.value) throw new Error("path is not defined")
-  delay.value = false
-
-  const baseAnimation = anime({
-    targets: path.value,
-    d: logoStages.map((stage) => ({ value: stage })),
-    easing: "linear",
-    autoplay: false,
-    duration,
-  })
-
-  anime({
-    targets: baseAnimation,
-    duration,
-    // direction: "alternate",
-    // loop: true,
-    update: (anim) => {
-      const eased = easing(anim.progress / 100)
-      const absolute = baseAnimation.duration * eased
-      const deadzone = duration / logoStages.length
-      const progress = range(
-        0,
-        baseAnimation.duration,
-        deadzone,
-        baseAnimation.duration,
-        absolute,
-      )
-
-      baseAnimation.seek(progress)
-    },
-  })
-})
 </script>
 
 <template>
-  <div class="vaaski-logo" :class="{ delay }">
+  <div class="vaaski-logo">
     <svg
       width="312"
       height="186"
@@ -76,23 +26,8 @@ onMounted(() => {
 </template>
 
 <style scoped>
-@keyframes fade-in {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-
 .vaaski-logo {
-  opacity: 0;
-  animation: fade-in 500ms forwards;
   position: relative;
-}
-
-.delay {
-  animation-delay: 500ms;
 }
 
 svg {
