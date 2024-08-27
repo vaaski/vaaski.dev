640a946dda90adf7eebf85729a50c4c67d107ef4
vaaski <admin@vaa.ski>
2024-08-27 21:43:22 +0200

    disable unicorn/no-anonymous-default-export for utils & functions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,12 @@
 import unicorn from "eslint-plugin-unicorn"
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-export default withNuxt().prepend([unicorn.configs["flat/recommended"]])
+export default withNuxt().prepend([
+	unicorn.configs["flat/recommended"],
+	{
+		files: ["utils/*.ts", "functions/*.mts"],
+		rules: {
+			"unicorn/no-anonymous-default-export": "off",
+		},
+	},
+])
